9a2173b55fc87d4d2b59aa152c45cf42b351120d
Rylan Schaeffer <rylanschaeffer@gmail.com>
2025-06-20 22:13:06 -0700

    Fixed outdated default in globals.py

--- src/globals.py
+++ src/globals.py
@@ -6,7 +6,7 @@ EVAL_DEFAULT_CONFIG = {
     # "model_hf_path": "Qwen/Qwen2.5-0.5B",
     "num_fewshot": 8,
     # "sampler": "min_p",
-    "sampler": "standard",
+    "sampler": "basic",
     "sampler_value": 0.0,
     # "sampler_value": 0.1,
     "seed": 0,
